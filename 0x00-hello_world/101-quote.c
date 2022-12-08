6dc9a43ca72ccaf204fa1d4b675bcb5044e0f0b4
MbaleeThing <zinhle.mbali@yahoo.com>
2022-12-08 15:11:50 -0800

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * main - entry point
- * Return: Always 1
+ *  * main - entry point
+ *  * Return: Always 1
  */
 int main(void)
 {
-		write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-		return (1);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+				return (1);
 }
