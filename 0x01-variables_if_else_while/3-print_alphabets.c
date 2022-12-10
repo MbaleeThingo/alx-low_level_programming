b75506f0c65a5611fe6ccfe68f211395f0f6acf5
MbaleeThing <zinhle.mbali@yahoo.com>
2022-12-09 18:36:00 -0800

    yoh

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet
+ * main - Prints the alphabe
  *
  * Return: Always 0 (success)
  */
@@ -14,6 +14,6 @@ for (i = 0; i < 52; i++)
 {
 putchar(alp[i]);
 }
-purchar('\n');
+putchar('\n');
 return (0);
 }
