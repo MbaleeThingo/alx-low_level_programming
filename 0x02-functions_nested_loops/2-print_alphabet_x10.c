a22cdd153a987cc3fd73d35cb7052292c53ffbe5
MbaleeThing <zinhle.mbali@yahoo.com>
2022-12-13 02:29:09 -0800

    two

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print ten times alphabet in lower case
+ * print_alphabet_x10 - print ten times alphabet, in lower case
  */
 
 void print_alphabet_x10(void)
@@ -13,4 +13,5 @@ while (count++ <= 9)
 for (letter = 'a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('/n');
+}
 }
