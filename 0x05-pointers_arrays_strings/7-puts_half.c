dba7e8af7418d19902993d73134aac8dba10aaa4
MbaleeThing <zinhle.mbali@yahoo.com>
2022-12-20 09:05:17 -0800

    seven

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,8 +3,8 @@
 /**
  * puts_half - a function that prints
  * half of a string, followed by a new line.
- *  @str - An input string
- *  Return: Nothing
+ * @str - An input string
+ * Return: Nothing
  */
 
 void puts_half(char *str)
@@ -18,6 +18,6 @@ c = a / 2;
 else
 c = (a + 1) / 2;
 for (b = c; b < a; b++)
-_putchar(str[b]);
+_putchar(str[b])
 _putchar('\n');
 }
