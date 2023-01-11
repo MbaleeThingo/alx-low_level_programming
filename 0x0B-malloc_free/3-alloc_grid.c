a4459469ca9bcaef51f2ce42c4428d41e52da1d5
MbaleeThing <zinhle.mbali@yahoo.com>
2023-01-11 01:56:00 -0800

    mollic

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,13 +22,17 @@ return (NULL);
 for (i = 0; i < height; i++)
 {
 a[i] = (int *)malloc(sizeof(int) * width);
-if (a[i] == NULL)															{
+if (a[i] == NULL)
+{
 for (j = 0; j < i; j++)
-free(a[j]);																free(a);
-return (NULL);																}
-for (j = 0; j < width; j++)
-{																	a[i][j] = 0;
+free(a[j]);
+free(a);
+return (NULL);
 }
-}								
+for (j = 0; j < width; j++)
+{
+a[i][j] = 0;
+}
+}
 return (a);
 }
