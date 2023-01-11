1d2b279d27b4929a41044626981675ebcf7fd657
MbaleeThing <zinhle.mbali@yahoo.com>
2023-01-11 02:49:10 -0800

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: input integer
+ * @argv: input character
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);
+}
