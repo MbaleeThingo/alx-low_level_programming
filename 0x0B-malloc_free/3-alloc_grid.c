f56c70043688cfc947c9a28cef639216c2d39d64
MbaleeThing <zinhle.mbali@yahoo.com>
2023-01-11 01:48:55 -0800

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,18 +19,16 @@ return (NULL);
 a = (int **)malloc(sizeof(int *) * height);
 if (a == NULL)
 return (NULL);
-for (i = 0; i < height; i++);
+for (i = 0; i < height; i++)
 {
 a[i] = (int *)malloc(sizeof(int) * width);
-if (a[i] == NULL)
-{
+if (a[i] == NULL)															{
 for (j = 0; j < i; j++)
-free(a[j]);
-free(a);																return (NULL);
+free(a[j]);																free(a);
+return (NULL);																}
+for (j = 0; j < width; j++)
+{																	a[i][j] = 0;
 }
-for (j = 0; j < width; j++)														{
-a[i][j] = 0;
-}
-}
+}								
 return (a);
 }
