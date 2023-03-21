0090afb858232083293302aa3d477e0c68501b07
MbaleeThing <zinhle.mbali@yahoo.com>
2023-03-21 01:09:37 -0700

    dynamics

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,104 @@
+#include "main.h"
+#include "aio.h"
+#include "string.h"
+#include "stddef.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+
+int _putchar(char c)
+{
+return (0);
+}
+
+int _islower(int c)
+{
+return (0);
+}
+
+int _isalpha(int c)
+{
+return (0);
+}
+
+int _abs(int n)
+{
+return (0);
+}
+
+int _isupper(int c)
+{
+ return (0);
+}
+
+
+int _isdigit(int c)
+{
+return (0);
+}
+
+
+int _strlen(char *s){
+return (0);
+}
+
+void _puts(char *s)
+{
+
+}
+
+char *_strcpy(char *dest, char *src)
+{
+return (0);
+}
+
+
+int _atoi(char *s)
+{
+return (0);
+}
+
+char *_strcat(char *dest, char *src)
+{
+return ("some string");
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+return (0);
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+return ("some string");
+}
+
+int _strcmp(char *s1, char *s2)
+{
+return (0);
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+return ("some string");
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return ("some string");
+}
+char *_strchr(char *s, char c)
+{
+return ("some string");
+}
+unsigned int _strspn(char *s, char *accept)
+{
+return (0);
+}
+char *_strpbrk(char *s, char *accept)
+{
+return ("some string");
+}
+char *_strstr(char *haystack, char *needle)
+{
+return ("some string");
+}
